181d0f763384881f42e6335b1e62b07947325ab5
abhaykagalkar <abhaykagalkar@gmail.com>
2019-10-14 22:47:29 +0530

    CPP template for competitive coding added

---
+++ cpp_template.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<map>
+#include<unordered_map>
+#include<utility>
+#include<algorithm>
+#include<climits>
+#include<set>
+#include<stack>
+#include<list>
+#include<set>
+#include<vector>
+#include<unordered_set>
+#include<iterator>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define MOD 10000007
+#define loop(x,k,n) for(int x=k,x<n;x++)
+#define revLoop(q,w) for(int q=w-1;q>=0;q--)
+#define ll long long int
+#define strInp(a) cin.getline(a,1000);
+#define mp(a,b) make_pair(a,b)
+#define init(a,n) memset(a,n,sizeof(a));
+#define pb(i) push_back(i);
+#define fastIo ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define tc int t; cin>>t; while(t--)
+#define arrInp(a,n) for(int i=0;i<n;i++) cin>>a[i]
+
+int main(){
+    fastIo;
+    tc{
+        //write your code here
+    }
+    return 0;
+}
